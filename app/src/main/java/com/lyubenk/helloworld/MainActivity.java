78bf15f0f672f743666b10aca07f6d3fa478b0f4
Lyuben Kodinov <lyuben.kodinov@gmail.com>
2022-06-13 22:14:38 +0300

    Read,write and delete functionality.

--- app/src/main/java/com/lyubenk/helloworld/MainActivity.java
+++ app/src/main/java/com/lyubenk/helloworld/MainActivity.java
@@ -1,17 +1,15 @@
 package com.lyubenk.helloworld;
 
-import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +27,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
-import java.util.Set;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -40,39 +37,30 @@ public class MainActivity extends AppCompatActivity {
     private TextView mRepCount;
     private TextView mWeightNumber;
     private TextView mWorkoutName;
+    private TextView mSelectedWorkout;
+    private int selectedWorkout = 0;
+    private File[] exerciseFiles;
     String tag = "Debugging";
-    private boolean allClickable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(tag,"in onCreate(Bundle).");
+
         mSetCount= (TextView) findViewById(R.id.set_count);
         mRepCount= (TextView) findViewById(R.id.rep_count);
         mWeightNumber= (TextView) findViewById(R.id.weight_number);
         mWorkoutName = (TextView) findViewById(R.id.workout_name);
+        mSelectedWorkout = (TextView) findViewById(R.id.selected_workout);
+        fetchExerciseFiles();
+
+        // Set selectedWorkoutView to first item (default)
+        setSelectedWorkout(0);
     }
     public void countSetUp(View view) {
         setSets(sets+1);
     }
-    public void countSetDown(View view) {
-        setSets(sets-1);
-    }
-    public void countRepUpBulk(View view){
-        setReps(Math.min(reps+5,99));
-    }
-    public void countRepUp(View view) {
-        setReps(reps+1);
-    }
-
-    public void countRepDown(View view) {
-        setReps(reps-1);
-    }
-    public void countRepDownBulk(View view){
-        setReps(Math.max(reps-5,0));
-    }
-
+    public void countSetDown(View view) { setSets(sets-1); }
     private void setSets(int number){
         if(number < 0 || number > 9) return;
         sets = number;
@@ -80,6 +68,17 @@ public class MainActivity extends AppCompatActivity {
             mSetCount.setText(Integer.toString(sets));
         }
     }
+
+    public void countRepUpBulk(View view){
+        setReps(Math.min(reps+5,99));
+    }
+    public void countRepUp(View view) {
+        setReps(reps+1);
+    }
+    public void countRepDown(View view) {
+        setReps(reps-1);
+    }
+    public void countRepDownBulk(View view){ setReps(Math.max(reps-5,0)); }
     private void setReps(int number){
         if(number < 0) return;
         reps = number;
@@ -87,6 +86,75 @@ public class MainActivity extends AppCompatActivity {
             mRepCount.setText(Integer.toString(reps));
         }
     }
+
+    public void selectNextWorkout(View view){ setSelectedWorkout(selectedWorkout+1);}
+    public void selectPreviousWorkout(View view){ setSelectedWorkout(selectedWorkout-1);}
+    public void setSelectedWorkout(int index){
+        if(exerciseFiles.length == 0){
+            mSelectedWorkout.setText("");
+            return;
+        }
+        else if(index < 0){
+            index = 0;
+        }
+        else if(index >= exerciseFiles.length){
+            index = exerciseFiles.length-1;
+        }
+        selectedWorkout = index;
+        String exerciseName = exerciseFiles[index].getName();
+        //                                              Remove .json at end
+        mSelectedWorkout.setText(exerciseName.substring(0,exerciseName.length()-5));
+    }
+
+    private Workout getWorkoutFromFields(){
+        Workout workout = new Workout();
+        workout.name = mWorkoutName.getText().toString();
+        workout.reps = Integer.parseInt(mRepCount.getText().toString());
+        workout.sets = Integer.parseInt(mSetCount.getText().toString());
+        workout.weight = Float.parseFloat(mWeightNumber.getText().toString());
+        return workout;
+    }
+    private void setFieldsFromWorkout(Workout workout){
+        mWorkoutName.setText(workout.name);
+        setReps(workout.reps);
+        setSets(workout.sets);
+        mWeightNumber.setText(Float.toString(workout.weight));
+    }
+    public void readIntoLayout(View view) throws FileNotFoundException {
+        File file = exerciseFiles[selectedWorkout];
+        // TODO: Replace with dynamic data structure
+        FileInputStream fis = this.openFileInput(file.getName());
+        InputStreamReader inputStreamReader =
+                new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            // Error occurred when opening raw file for reading.
+        } finally {
+            String contents = stringBuilder.toString();
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            Workout workout = gson.fromJson(contents,Workout.class);
+            setFieldsFromWorkout(workout);
+        }
+    }
+    private void fetchExerciseFiles(){
+        HashSet<File> names = new HashSet<>();
+        File[] files = getFilesDir().listFiles();
+        for(int i =0;i<files.length;++i){
+            if(files[i].getName().contains(".json")){
+                names.add(files[i]);
+            }
+        }
+        exerciseFiles = new File[names.size()];
+        names.toArray(exerciseFiles);
+    }
+
     public void weightCursorReset(View view){
         EditText et = (EditText)view;
         et.setSelection(et.length());
@@ -131,33 +199,30 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
         Log.d(tag,"in onDestroy().");
     }
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public void read(View view){
-        Toast toast = new Toast(this);
-
-        File[] files = getFilesDir().listFiles();
-        Set<String> list = new HashSet<>();
-        for(int i =0;i<files.length;++i){
-            if(files[i].getName().contains(".json")){
-                list.add(files[i].getName());
-            }
-        }
-        toast.setText("Files found: "+ list);
-        toast.show();
+    public void clearForViews(View view){clear();}
+    public void clear(){
+        setReps(0);
+        setSets(0);
+        mWorkoutName.setText("");
+        mWeightNumber.setText("");
     }
     public void save(View view){
+        Toast toast = new Toast(this);
+        if (mWorkoutName.getText().length() == 0) {
+            toast.setText("Couldn't save because of invalid name.");
+            toast.show();
+            return;
+        } else if (reps == 0 || sets == 0) {
+            toast.setText("Reps and sets need to be at least one.");
+            toast.show();
+            return;
+        }
         File file = new File(this.getFilesDir(), mWorkoutName.getText().toString() + ".json");
 
         try {
-            Workout workout = new Workout();
-            workout.name = mWorkoutName.getText().toString();
-            workout.reps = Integer.parseInt(mRepCount.getText().toString());
-            workout.sets = Integer.parseInt(mSetCount.getText().toString());
-            workout.weight = Float.parseFloat(mWeightNumber.getText().toString());
-
             GsonBuilder builder = new GsonBuilder();
             Gson gson = builder.create();
-            String data = gson.toJson(workout);
+            String data = gson.toJson(getWorkoutFromFields());
 
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(file));
             outputStreamWriter.write(data);
@@ -166,7 +231,6 @@ public class MainActivity extends AppCompatActivity {
         catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
-        Toast toast = new Toast(this);
         toast.setText("Successfully saved exercise "+mWorkoutName.getText().toString());
         toast.show();
 
@@ -191,5 +255,40 @@ public class MainActivity extends AppCompatActivity {
         } finally {
             Log.d(tag,"Read successfully: "+ stringBuilder.toString());
         }
+    }
+    public void delete(View view){
+        String filename = mWorkoutName.getText().toString();
+        if(filename.isEmpty()){
+            Toast toast = new Toast(this);
+            toast.setText("Cannot delete a nameless file.");
+            toast.show();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure you want to delete '"+filename+"'?");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                deleteFile(exerciseFiles[selectedWorkout].getName());
+                Log.d("Deleting...","Deleting "+filename+".");
+                fetchExerciseFiles();
+                setSelectedWorkout(selectedWorkout-1);
+                clear();
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
